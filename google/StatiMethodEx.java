d772c23099a429259cf578f4b430e7c4fe73c1fc
GaneshPC-018 <2023bit501@sggs.ac.in>
2024-02-04 14:20:59 +0530

    hello Committed on 7 Feb

--- google/StatiMethodEx.java
+++ google/StatiMethodEx.java
@@ -2,6 +2,7 @@ package google;
 public class StatiMethodEx {
     int income = 1000;
     static int income2 = 2000;
+    String name;
     static void methodData()
     {
         StatiMethodEx o1 = new StatiMethodEx();
@@ -9,9 +10,15 @@ public class StatiMethodEx {
         System.out.println(income2);
     }
     public static void main(String[] args) {
-        StatiMethodEx.methodData();//Class Member
-        methodData(); //direct
+        // StatiMethodEx.methodData();//Class Member
+        // methodData(); //direct
         StatiMethodEx obj = new StatiMethodEx();
-        obj.methodData();//can be access using object too
+        // obj.methodData();//can be access using object too
+        System.out.println(obj.name);
+        // System.out.println(obj.name.length());
+        // long pankaj = 1000; //8 Byte
+        // int a = (int)pankaj;//typeCast 
+        
+
     }
 }
